ea647d45a814d1b7996fdf2bbdd30209b45ad674
王玉鹏 <550902023@qq.com>
2020-05-14 23:00:49 +0800

    整理代码

--- src/main/java/com/triumphxx/config/MybatisPlusConfig.java
+++ src/main/java/com/triumphxx/config/MybatisPlusConfig.java
@@ -8,29 +8,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
- * @author:triumphxx
- * @Date:2020/5/10
- * @Time:2:35 下午
+ * @author:triumphxx @Date:2020/5/10 @Time:2:35 下午
  * @desc:mybatis-plus配置文件
- **/
+ */
 @Configuration
 @EnableTransactionManagement
 @MapperScan("com.triumphxx.mapper")
 public class MybatisPlusConfig {
 
-    /**
-     * mybatis 分页插件
-     * @return
-     */
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
-        // paginationInterceptor.setOverflow(false);
-        // 设置最大单页限制数量，默认 500 条，-1 不受限制
-        // paginationInterceptor.setLimit(500);
-        // 开启 count 的 join 优化,只针对部分 left join
-        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
-        return paginationInterceptor;
-    }
+  /**
+   * mybatis 分页插件
+   *
+   * @return
+   */
+  @Bean
+  public PaginationInterceptor paginationInterceptor() {
+    PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+    // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+    // paginationInterceptor.setOverflow(false);
+    // 设置最大单页限制数量，默认 500 条，-1 不受限制
+    // paginationInterceptor.setLimit(500);
+    // 开启 count 的 join 优化,只针对部分 left join
+    paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+    return paginationInterceptor;
+  }
 }
